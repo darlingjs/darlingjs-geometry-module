135003b8019532d643baebb5742cdaa310b2771d
Eugene Krevenets <hyzhak@ya.ru>
2013-07-16 09:07:30 +0200

    add selected component and follow system

--- flatland.js
+++ flatland.js
@@ -48,6 +48,42 @@
         height: 480
     });
 
+    m.$c('ngSelected');
+
+    m.$s('ngFollowSelected', {
+        _avgPosition: {x:0.0, y:0.0},
+
+        shift: {
+            x: 0.0,
+            y: 0.0
+        },
+
+        $require: ['ng2D', 'ngSelected'],
+
+        $beforeUpdate: function() {
+            this._avgPosition.x = 0.0;
+            this._avgPosition.y = 0.0;
+            this._avgPosition.count = 0;
+        },
+
+        $update: ['$entity', function($entity) {
+            this._avgPosition.x += $entity.ng2D.x;
+            this._avgPosition.y += $entity.ng2D.y;
+            this._avgPosition.count++;
+        }],
+
+        $afterUpdate: ['ng2DViewPort', function(ng2DViewPort) {
+            if (this._avgPosition.count > 1) {
+                var coef = 1 / this._avgPosition.count;
+                this._avgPosition.x *= coef;
+                this._avgPosition.y *= coef;
+            }
+
+            ng2DViewPort.lookAt.x = this._avgPosition.x + this.shift.x;
+            ng2DViewPort.lookAt.y = this._avgPosition.y + this.shift.y;
+        }]
+    });
+
     /**
      * Marker for elements that doesn't influenced by the viewPort
      */
